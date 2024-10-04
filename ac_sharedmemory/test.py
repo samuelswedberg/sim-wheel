b4dfe0bc450ffad890a2d47bb17656aa5b1ea62c
samuelswedberg <duckliningdon@protonmail.com>
2024-10-03 21:20:36 -0500

    Progress on UART communication

--- ac_sharedmemory/test.py
+++
@@ -1,9 +0,0 @@
-from pyaccsharedmemory import accSharedMemory
-
-asm = accSharedMemory()
-
-while(1):
-    sm = asm.read_shared_memory()
-    if (sm is not None):
-        print(f"Gear: {sm.Physics.rpm}")
-    
