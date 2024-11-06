44446bd1a86c0f066e3ceb216163135f03e8040d
samuelswedberg <duckliningdon@protonmail.com>
2024-11-05 19:38:25 -0600

    Docs updated

--- ac_sharedmemory/uart.py
+++ ac_sharedmemory/uart.py
@@ -2,6 +2,7 @@ from pyaccsharedmemory import accSharedMemory
 import serial
 import time
 import json
+import sys
 
 asm = accSharedMemory()
 
@@ -20,6 +21,19 @@ def send_json_data():
     "brakeBias": int(sm.Physics.brake_bias),
     "forceFB": round(sm.Physics.final_ff, 2)
     }
+    # data = { # Demo Data
+    # "rpm": 3600,
+    # "gear": 1,
+    # "speedKmh": 120,
+    # "hasDRS": 0,
+    # "drs": 0,
+    # "pitLim": 0,
+    # "fuel": 45,
+    # "brakeBias": 0,
+    # "forceFB": 0.1
+    # }
+
+    print_data(data)
     # serialize the dictionary to a JSON string
     json_data = json.dumps(data) + '\n'  # Add a newline for message termination
     # Send the JSON string over UART
@@ -30,6 +44,17 @@ def receive_data():
     if usart.in_waiting > 0:  # Check if there is data to read
         raw_line = usart.readline().decode('utf-8').rstrip()  # Read a line and decode it
         print("Received:", raw_line)  # Print the received data
+
+def print_data(data):
+    """Function to print the data struct in a single, updating line."""
+    # Clear the line and move cursor to the beginning
+    sys.stdout.write("\r")  
+    # Format the data as a single string
+    formatted_data = " | ".join([f"{key}: {value}" for key, value in data.items()])
+    # Print the formatted data
+    sys.stdout.write(formatted_data)
+    # Flush the output to update the line immediately
+    sys.stdout.flush()   
 
 if __name__ == "__main__":
     try:
